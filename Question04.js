d8c64ca1c4af69e6cf113f7c86a584288edc9388
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-03-13 19:47:08 +0530

    Reverse the numbers in left and right

---
+++ Question04.js
@@ -0,0 +1,18 @@
+console.log("**Reverse the numbers in left and right 4321-5-9876**")
+
+let num =9;
+        
+let mid =(parseInt(num/2))
+
+        for(let i = mid; i >= 1; i--){
+        console.log(i);
+        }
+
+        console.log(mid+1);
+
+        for(let i =num ;i > mid+1;i-- ){
+            console.log(i);
+            }
+            
+
+        console.log();
