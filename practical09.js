dc916a61af4d3dad3057daea76580eff73bc3431
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-03-19 21:57:03 +0530

    JSON

---
+++ practical09.js
@@ -0,0 +1,13 @@
+//JSON
+//{key:value}
+let student = {regno: '2021/ICT/01', name:'James', age:21, Course: 'IT', Skils:["java", "JS", "C++"]}
+console.log(student)
+console.log(student.name)
+
+let students = [
+{regno: '2021/ICT/01', name:'James', age:21, Course: 'IT'},
+{regno: '2021/ICT/02', name:'Tom', age:22, Course: 'IT'},
+{regno: '2021/ICT/03', name:'Anna', age:21, Course: 'IT'}
+]
+
+console.log(students)
