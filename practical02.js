e5bb77b8bf67f858a1fba31c6fe842127240afa2
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-03-19 21:47:55 +0530

    Print the nested Array

---
+++ practical02.js
@@ -0,0 +1,10 @@
+// //print the nested Array
+// //[[1,2,3],[5,6],[8,5,3]]
+
+let arr1 = [[1, 2, 3],[5, 6],[8, 5, 3]]
+
+arr1.forEach((n) =>{
+    n.forEach((i) =>{
+        console.log(i)
+    })
+})
